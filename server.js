607e60756256e8a18a9ecc0cb3371bce418f2ab7
blur <damienhebert710@gmail.com>
2022-05-14 13:53:37 -0500

    setup static route

--- server.js
+++ server.js
@@ -2,8 +2,14 @@ const express = require('express')
 const fs = require('fs')
 const path = require('path')
 
-const PORT = 3001
+const PORT = 3002
 const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended:true }))
+app.use(express.static('public'))
+
+
 
 app.get('/notes', (req,res) =>
 res.send(path.join(__dirname, '/public/notes.html')))
