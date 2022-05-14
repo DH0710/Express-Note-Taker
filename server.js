b94ba3d17a3fdaca14d3a0ea25e7de551d5441f3
blur <damienhebert710@gmail.com>
2022-05-14 13:35:53 -0500

    new routes added

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const PORT = 3001
 const app = express()
 
 app.get('/notes', (req,res) =>
-res.send(__dirname, '/public/notes.html'))
+res.send(path.join(__dirname, '/public/notes.html')))
+
+app.get('*', (req,res) => 
+res.send(path.join(__dirname, '/public/index.html')))
 
 app.listen(PORT, () => console.log(`app is listening on http://localhost:${PORT}`) )
